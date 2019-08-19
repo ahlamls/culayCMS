27da7125351adff171afd23615925581afb60dfd
ahlamls <fulldoang@gmail.com>
2019-08-19 16:07:55 +0700

    apdet db

--- culay.sql
+++ culay.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Aug 18, 2019 at 11:38 PM
+-- Generation Time: Aug 19, 2019 at 04:07 PM
 -- Server version: 5.7.27-0ubuntu0.18.04.1
 -- PHP Version: 7.0.33-10+ubuntu18.04.1+deb.sury.org+1
 
@@ -63,7 +63,8 @@ INSERT INTO `kota` (`id`, `nama`, `gambar`) VALUES
 (2, 'Bandung', 'data/image/bandung.jpg'),
 (3, 'Jakarta', 'data/image/jakarta.jpg'),
 (4, 'Yogyakarta', 'data/image/jogja.jpg'),
-(5, 'Bogor', 'data/image/bogor.png');
+(5, 'Bogor', 'data/image/bogor.png'),
+(7, 'Pekalongan', 'data/image/pekalongan.png');
 
 -- --------------------------------------------------------
 
@@ -90,8 +91,35 @@ CREATE TABLE `produk` (
 
 INSERT INTO `produk` (`id`, `judul`, `desk`, `harga`, `type`, `qty`, `gambar`, `kota_id`, `kat_id`, `user_id`) VALUES
 (1, 'Dompet', 'Dompet dari luar kota', 100000, 1, 1, 'data/image/dompnyut.jpg', 1, 4, 1),
-(2, 'Kadal Gurun', 'Kadal Gurun import dari miramar . sekarang tersedia di bogor!', 80000000, 2, 1, 'data/image/kadal.jpeg', 5, 2, 2),
-(4, 'Pedas ', 'hasil import dari malingsial ambil di jakarta', 100000, 0, 1, 'data/image/2019-08-18_17607.5418793933.jpg', 3, 1, 1);
+(2, 'Kadal Gurun', 'Kadal Gurun import dari miramar . sekarang tersedia di bogor!', 800000, 0, 1, 'data/image/kadal.jpeg', 5, 2, 2),
+(5, 'Rumah Tanpa Pintu', 'rumah tanpa pintu persis seperti gim free fayer hasil impor india ', 690000000, 1, 1, 'data/image/rumah-tanpa-pintu-di-india_20181008_185731.jpg', 4, 5, 1),
+(7, 'Cheat Point Blank Wallhack Anti Ban', 'lagi butuh cit pb dari pekalongan', 50000, 0, 1, 'data/image/CKbhc4iUwAAsG33.jpg', 7, 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id` int(11) NOT NULL,
+  `penerima` text NOT NULL,
+  `nama` text NOT NULL,
+  `alamat` text NOT NULL,
+  `produk_id` int(11) NOT NULL,
+  `penjual_id` int(11) NOT NULL,
+  `pembeli_id` int(11) NOT NULL,
+  `status` text NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id`, `penerima`, `nama`, `alamat`, `produk_id`, `penjual_id`, `pembeli_id`, `status`, `harga`) VALUES
+(3, 'Baon Kicadap', 'Kadal Gurun', 'Jl . asede no 69', 2, 2, 1, 'Menunggu Diproses Penjual', 800000),
+(4, 'Amak Kalian', 'jl brikasap femeli', 'Cheat Point Blank Wallhack Anti Ban', 7, 1, 2, 'Menunggu Diproses Penjual', 50000);
 
 -- --------------------------------------------------------
 
@@ -113,8 +141,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `name`, `email`, `password`, `saldo`) VALUES
-(1, 'baoncikadap', 'Baon Cikadap', 'baon@cikadap.com', 'e607cf1d9200895b67a77528c385fd01', 0),
-(2, 'bstrax', 'B Strax', 'bstrax@amakkalian.com', 'e607cf1d9200895b67a77528c385fd01', 1000);
+(1, 'baoncikadap', 'Baon Cikadap', 'baon@cikadap.com', 'e607cf1d9200895b67a77528c385fd01', 300000),
+(2, 'bstrax', 'B Strax', 'bstrax@amakkalian.com', 'e607cf1d9200895b67a77528c385fd01', 701000);
 
 --
 -- Indexes for dumped tables
@@ -139,6 +167,12 @@ ALTER TABLE `produk`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -157,11 +191,16 @@ ALTER TABLE `kategori`
 -- AUTO_INCREMENT for table `kota`
 --
 ALTER TABLE `kota`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `user`
