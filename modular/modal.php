212a46fd2da0447b1de198f5c5863b6d24bb2208
ahlamls <fulldoang@gmail.com>
2019-08-20 11:38:27 +0700

    fix register

--- modular/modal.php
+++ modular/modal.php
@@ -35,7 +35,7 @@
 
 <!-- Modal -->
 <div class="modal fade" id="RegModal" tabindex="-1" role="dialog" aria-labelledby="RegModalTitle" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog modal-dialog-scrollable" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="RegModalTitle">Daftar</h5>
